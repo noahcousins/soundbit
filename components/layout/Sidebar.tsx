2af4296e21b26788c0df27a1c5a603a8d58520a9
Noah Cuz <sales@noahcuz.com>
2024-01-04 10:14:23 -0500

    bug fixes

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,10 +1,5 @@
-import ModeToggle from '@/components/ModeToggle';
-import NavLink from '@/components/layout/NavLink';
-import SidebarAuthButtons from '@/components/layout/SidebarAuthButtons';
-import SignOutButton from '@/components/layout/SignOutButton';
 import SidebarContent from '@/components/layout/sidebar/SidebarContent';
-import { buttonVariants } from '@/components/ui/button';
-import { Separator } from '@/components/ui/separator';
+
 import {
   Users2,
   ScrollText,
